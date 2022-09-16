929050b87d53c0632e14bedc87b41a7de281012d
Eggy fhr <22157337+iameggi@users.noreply.github.com>
2022-09-17 00:22:57 +0700

    Update Materi.go

--- tipe-data/Materi.go
+++ tipe-data/Materi.go
@@ -1,1 +1,10 @@
-C:\Users\Administrator\evp-erp-v2\Loadmodels.go
+package main
+
+import "fmt"
+
+func main() {
+	var phi float64 = 3.1456346
+	var nama string = "Eggy"
+	fmt.Printf("Phi %.2f", phi)     //mengprint dengan arugument 2f berati mengambil 2 dari belakang
+	fmt.Printf("\n Nama: %s", nama) // arugument $s = string \n = newline
+}
